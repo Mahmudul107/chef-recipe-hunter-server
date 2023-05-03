5e66fc456a49f077e3a6a4d5ba3a75c4bb398f11
Mahmudul107 <mahmudulislam378@gmail.com>
2023-05-03 20:13:34 +0600

    Chefs data with json added

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+const chefsData = require('./data/chefsData.json')
+
 app.get('/', (req, res) => {
-    req.send('server is  listening')
+    res.send('server is  listening')
 });
+app.get('/chefs', (req, res) => {
+    res.send(chefsData)
+})
 
 app.listen(port, () =>{
     console.log(`listening on port: ${port}`);
